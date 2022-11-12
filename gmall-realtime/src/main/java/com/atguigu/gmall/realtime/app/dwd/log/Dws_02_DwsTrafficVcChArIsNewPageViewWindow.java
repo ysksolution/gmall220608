51b050c1109610cb7e75a880f02595bdc1c81037
lzc <aa@souhu.com>
2022-11-12 08:26:52 +0800

    update

---
+++ gmall-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwd/log/Dws_02_DwsTrafficVcChArIsNewPageViewWindow.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.realtime.app.dwd.log;
+
+/**
+ * @Author lzc
+ * @Date 2022/11/12 08:26
+ */
+public class Dws_02_DwsTrafficVcChArIsNewPageViewWindow {
+    public static void main(String[] args) {
+    
+    }
+}
+/*
+
+*/
